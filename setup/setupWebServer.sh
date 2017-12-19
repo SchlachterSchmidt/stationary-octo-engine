352ef9f87c87f24bd5f1cd8069063f65f31e52f6
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-19 23:42:07 +0000

    moved environment management to conda added installation of additional libraries

--- setup/setupWebServer.sh
+++ setup/setupWebServer.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/bin/bash
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -6,70 +6,84 @@ if [ "$EUID" -ne 0 ]
 fi
 
 ip=$(wget http://ipinfo.io/ip -qO -)
+echo "my IP: $ip"
 
-echo "$ip"
+# create project folder
+dir=~/finalProject
+if [ ! -d  "$dir" ]; then
+    mkdir $dir;
+fi;
+cd $dir
 
-echo PYTHON ENVIRONMENT
-sudo apt-get update
-sudo apt-get install python3-pip python3-dev nginx
-sudo pip3 install virtualenv
-mkdir ~/myproject
-cd ~/myproject
-virtualenv myprojectenv
-source myprojectenv/bin/activate
-pip install gunicorn flask
+echo ANACONDA ENVIRONMENT
+cd ~
+wget https://repo.continuum.io/archive/Anaconda3-5.0.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.0.1-Linux-x86_64.sh -b -p ~/anaconda
+rm Anaconda3-5.0.0.1-Linux-x86_64.sh
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
+sleep 2
+source .bashrc
+sleep 3
+echo conda installed successfully
+echo updating conda packages, press y to confirm
+sleep 2
+conda update conda
+conda update anaconda
+conda create -n finalProject python=3.6 anaconda
+source activate finalProject
+conda install pip numpy scipy theano scikit-learn jupyter flask nginx gunicorn
+conda install -c conda-forge bcolz
+pip install keras
+
+echo CREATING SAMPLE FLASK app
 
 echo "from flask import Flask
 app = Flask(__name__)
-
 @app.route(\"/\")
 def hello():
     return \"<h1 style='color:blue'>Hello World!</h1>\"
-
 if __name__ == \"__main__\":
-    app.run(host='0.0.0.0')" > ~/myproject/myproject.py
+    app.run(host='0.0.0.0')" > ~/finalProject/app.py
 
-echo GUNICORN CONFIG
+echo CREATING GUNICORN CONFIG
 
-echo "from myproject import app
-
+echo "from app import app
 if __name__ == \"__main__\":
-    app.run()" > ~/myproject/wsgi.py
+    app.run()" > ~/finalProject/wsgi.py
 
-deactivate
 
-echo SYSTEMD Unit File
+echo DEACTIVATING CONDA ENVIRONMENT
+source deactivate
+
+echo SYSTEMD UNIT FILE
 echo "[Unit]
-Description=Gunicorn instance to serve myproject
+Description=Gunicorn instance to serve finalProject
 After=network.target
-
 [Service]
 User=ubuntu
 Group=www-data
-WorkingDirectory=/home/ubuntu/myproject
-Environment=\"PATH=/home/ubuntu/myproject/myprojectenv/bin\"
-ExecStart=/home/ubuntu/myproject/myprojectenv/bin/gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app
-
+WorkingDirectory=/home/ubuntu/finalProject
+Environment=\"PATH=/home/ubuntu/anaconda/finalproject/bin\"
+ExecStart=/home/ubuntu/anaconda/finalproject/bin/gunicorn --workers 3 --bind unix:finalProject.sock -m 007 wsgi:app
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/myproject.service
+WantedBy=multi-user.target" > /etc/systemd/system/finalProject.service
 
-sudo systemctl start myproject
-sudo systemctl enable myproject
+sudo systemctl start finalProject
+sudo systemctl enable finalProject
 
 echo "server {
     listen 80;
     server_name $ip;
-
     location / {
         include proxy_params;
-        proxy_pass http://unix:/home/ubuntu/myproject/myproject.sock;
+        proxy_pass http://unix:/home/ubuntu/finalProject/finalProject.sock;
     }
-}" > /etc/nginx/sites-available/myproject
+}" > /etc/nginx/sites-available/finalProject
 
-sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/finalProject /etc/nginx/sites-enabled
 sudo systemctl restart nginx
 sudo ufw allow 'Nginx Full'
 
-echo "python myproject.py to test flask app"
+echo "python app.py to test flask app"
 echo "gunicorn --bind 0.0.0.0:5000 wsgi:app to test gunicorn"
 echo "sudo nginx -t to test Nginx"
