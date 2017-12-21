417fcae3da891703260ae6095a069f276f11fe4f
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-21 22:53:52 +0000

    basic api functionality

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+"""REST API prividing access to webserver resources for mobile agents."""
+
+from flask import Flask, make_response, jsonify
+
 app = Flask(__name__)
-@app.route("/")
+
+
+@app.route('/api/v0.1/hello', methods=['GET'])
 def hello():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    """Hello World API call."""
+    return make_response(jsonify({'hello': 'world'}), 200)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler to build 404 in JSON."""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
