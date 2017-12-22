5743a4141bb61be9c56a1fa2f8b0798ae55f6d4d
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-22 09:49:14 +0000

    update postgres install script

--- setup/postgresInstall.sh
+++ setup/postgresInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # install new postgres database server
+echo "Installing Postgres"
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update
@@ -10,3 +11,9 @@ echo "sudo -u postgres psql <- to connect to database"
 echo "sudo -u postgres <- to become postgres user, exit to logout"
 echo "\conninfo <- for connection info (database name, user, socket, port)"
 echo "\q <- to disconnect"
+
+
+echo "Installing SQLAlchemy and psycopg2 into conda environment"
+conda install psycopg2
+conda install -c conda-forge flask-sqlalchemy
+conda install -c conda-forge flask-migrate 
