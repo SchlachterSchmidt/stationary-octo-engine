9a34a34d97981eeb1abd68cc90b8a235620c82c4
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-23 18:34:02 +0000

    add ImageLink model

--- app/models.py
+++ app/models.py
@@ -10,7 +10,23 @@ class User(db.Model):
     lastname = db.Column(db.String(64), index=True, nullable=False)
     email = db.Column(db.String(64), index=True, nullable=False)
     username = db.Column(db.String(64), index=True, nullable=False)
+    images = db.relationship('ImageLink', backref='Creator', lazy=True)
 
     def __repr__(self):
         """User representation."""
         return '<user %r>' % (self.username)
+
+
+class ImageLink(db.Model):
+    """Link to images associated with a user."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id'), index=True, nullable=False)
+    link = db.Column(db.String(200), index=True, nullable=False)
+    predicted_label = db.Column(db.String(10), index=True, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        """Image representation."""
+        return self.link
