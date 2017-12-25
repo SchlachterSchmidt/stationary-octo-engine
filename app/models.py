652042b03e78e5f42434a579cc29a874da80c31f
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2017-12-25 19:18:34 +0000

    add uniqueness constraint on username and email user model

--- app/models.py
+++ app/models.py
@@ -8,8 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(64), index=True, nullable=False)
     lastname = db.Column(db.String(64), index=True, nullable=False)
-    email = db.Column(db.String(64), index=True, nullable=False)
-    username = db.Column(db.String(64), index=True, nullable=False)
+    email = db.Column(db.String(64), index=True, nullable=False, unique=True)
+    username = db.Column(db.String(64), index=True, nullable=False, unique=True)
     password_hash = db.Column(db.String(120), nullable=False)
     images = db.relationship('ImageLink', backref='Creator', lazy=True)
 
