d158beb21d15f06cc31c027f246594b325643cfe
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-04 15:27:02 +0000

    modified model path

--- app/classifier.py
+++ app/classifier.py
@@ -4,5 +4,6 @@ from pathlib import Path
 from keras.models import load_model
 
 
-model_def = str(Path.cwd().parent.joinpath('static/CNN_two_convs_30122017.h5'))
+model_def = str(Path.cwd().joinpath('static/CNN_two_convs_30122017.h5'))
 model = load_model(model_def)
+model.summary()
