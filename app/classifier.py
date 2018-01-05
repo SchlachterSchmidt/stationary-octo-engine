59e1b26840219289b1f085d921f0f54b9c02c24a
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-05 14:18:23 +0000

    update classify method, to swap axes one more time and add comments to better explain behaviour

--- app/classifier.py
+++ app/classifier.py
@@ -11,21 +11,32 @@ class Classifier:
 
     def __init__(self):
         """Classifier constructor."""
-        model_def = str(Path.cwd().joinpath('static/CNN_two_convs_30122017.h5'))
+        model_def = str(Path.cwd().joinpath(
+                        'static/CNN_two_convs_30122017.h5'))
         self.model = load_model(model_def)
         self.model.summary()
 
-
-    def classify(self, image):
-        """Classify a provided image file."""
-        x = np.fromstring(image, dtype='uint8')
-        print(x.shape)
-        img = cv2.imdecode(x, cv2.IMREAD_UNCHANGED)
-        np_image = cv2.resize(img, (224, 224))
+    def classify(self, image_stream_data):
+        """Classify image file stream."""
+        # reading the image stream data as 8 bit binary data into ndarray
+        np_data_arr = np.fromstring(image_stream_data, dtype='uint8')
+        print(np_data_arr.shape)
+        # decode ndarray to image with BGR channels (Inferred from data)
+        raw_img = cv2.imdecode(np_data_arr, cv2.IMREAD_UNCHANGED)
+        # resizing image to 224 *224 pixels
+        np_image = cv2.resize(raw_img, (224, 224))
         print(np_image.shape)
-        np_image = np.expand_dims(np_image, axis=0)
-        print(np_image.shape)
-        np_image = np.swapaxes(np_image, 1, 3)
-       	print(np_image.shape) 
-        cv2.imwrite('filename.jpg', np_image)
-        return self.model.predict(np_image).tolist()
+        # the classifier expected input format is:
+        # (batch_size, channels, x_dims, y_dims). In this case, batch size = 1
+        # performing two operations:
+        # - add 4th dimension
+        # - swap axes to put channels into right order
+        # first, adding 4th dim:
+        np_image_tensor = np.expand_dims(np_image, axis=0)
+        print(np_image_tensor.shape)
+        # second, converting to channels first ordering
+        np_image_tensor = np.swapaxes(np.swapaxes(np_image_tensor, 1, 3), 2, 3)
+        print(np_image_tensor.shape)
+        # making prediction on the image and returning probabilities
+        # for each class as list (as ndarray are not jsonify-able)
+        return self.model.predict(np_image_tensor).tolist()
