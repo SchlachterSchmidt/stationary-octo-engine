d534ee8a7e9c87e447621d503a61ecce033eae37
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-06 17:35:43 +0000

    remove debug print messages

--- app/classifier.py
+++ app/classifier.py
@@ -20,23 +20,19 @@ class Classifier:
         """Classify image file stream."""
         # reading the image stream data as 8 bit binary data into ndarray
         np_data_arr = np.fromstring(image_stream_data, dtype='uint8')
-        print(np_data_arr.shape)
         # decode ndarray to image with BGR channels (Inferred from data)
         raw_img = cv2.imdecode(np_data_arr, cv2.IMREAD_UNCHANGED)
         # resizing image to 224 *224 pixels
         np_image = cv2.resize(raw_img, (224, 224))
-        print(np_image.shape)
         # the classifier expected input format is:
         # (batch_size, channels, x_dims, y_dims). In this case, batch size = 1
         # performing two operations:
         # - add 4th dimension
-        # - swap axes to put channels into right order
+        # - swap axes to put dimensions into right order
         # first, adding 4th dim:
         np_image_tensor = np.expand_dims(np_image, axis=0)
-        print(np_image_tensor.shape)
         # second converting to channels first ordering
         np_image_tensor = np.swapaxes(np.swapaxes(np_image_tensor, 1, 3), 2, 3)
-        print(np_image_tensor.shape)
         # making prediction on the image and returning probabilities
         # for each class as list (as ndarray are not jsonify-able)
         return self.model.predict(np_image_tensor).tolist()
