d88e8d9c2a7e76e8d63ca79d08b2a77910e34d83
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-13 23:42:50 +0000

    adding s3 config to all config subclasses

--- config.py
+++ config.py
@@ -14,17 +14,24 @@ class Config(object):
 
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
+    S3_KEY = os.environ['S3_ACCESS_KEY_ID']
+    S3_SECRET = os.environ['S3_SECRET_ACCESS_KEY']
+
 
 class TestConfig(Config):
     """Config class for test environment."""
 
     SQLALCHEMY_DATABASE_URI = os.environ['TEST_DATABASE_URL']
+    S3_BUCKET = os.environ['TEST_S3_BUCKET']
+    S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 
 class DevConfig(Config):
     """Config class for dev environment."""
 
     SQLALCHEMY_DATABASE_URI = os.environ['DEV_DATABASE_URL']
+    S3_BUCKET = os.environ['DEV_S3_BUCKET']
+    S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 
 class PrdConfig(Config):
@@ -33,6 +40,8 @@ class PrdConfig(Config):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ['PRD_DATABASE_URL']
+    S3_BUCKET = os.environ['PRD_S3_BUCKET']
+    S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 
 app_config = {
