7dd81ad5a42d1c824519806bb90c50850cfe5b99
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-16 11:53:50 +0000

    import db from models module instead of app

--- migrate.py
+++ migrate.py
@@ -2,7 +2,8 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app import create_app, db
+from app import create_app
+from app.models import db
 
 app = create_app(config_mode='development')
 
