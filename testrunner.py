22f92427b9e7e88a222fb797e98429c2acd09b38
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-16 16:15:31 +0000

    move testcase to /tests dir and add testrunner to discover and exec all test cases

---
+++ testrunner.py
@@ -0,0 +1,23 @@
+"""Testrunner discovering and running all tests in /tests dir."""
+
+import unittest
+from flask_script import Manager
+
+from app import create_app
+
+app = create_app(config_mode='testing')
+manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Run the unit tests in /tests dir."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    manager.run()
