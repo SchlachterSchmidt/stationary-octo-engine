44ccfaf7f71acf83065b7e7a56198aaebf397682
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-21 15:31:52 +0000

    simplified aggregator

--- app/helpers/aggregator.py
+++ app/helpers/aggregator.py
@@ -1,67 +1,39 @@
-"""Aggregates a series of classifications into an attention score.
+"""Aggregates a classification and the previous score into an updated score.
 
 class: label: distractiveness
 c0: safe driving: 1
-c1: texting - right: 10
-c2: talking on the phone - right: 7
-c3: texting - left: 10
-c4: talking on the phone - left: 7
-c5: operating the radio: 6
-c6: drinking: 5
-c7: reaching behind: 15
-c8: hair and makeup: 8
-c9: talking to passenger: 5
+c1: texting - right: 7
+c2: talking on the phone - right: 6
+c3: texting - left: 7
+c4: talking on the phone - left: 6
+c5: operating the radio: 5
+c6: drinking: 4
+c7: reaching behind: 10
+c8: hair and makeup: 7
+c9: talking to passenger: 3
 
 
-n - 4 contrubutes 20%
-n - 3 contributes 40%
-n - 2 contributes 60%
-n - 1 contributes 80%
-n  contributes 100%
-
-scores are summed and divided by the count of scores and then a sigmoid
-function is applied, shifted by +5
-
-formula
-n[1-4] = distractiveness * confidence of prediction
-x = ((n-4*0.2)+(n-3*0.4)+(n-2*0.6)+(n-1*.08)+(n))
-score = 1/(1 + e^(-x+5))
-
-The worst possible score is 0.98, the best possible score 0.01
+current score is the class penalty multiplied by  confidence
+previous score is added, and sum is divided by 2
 """
 
-import math
 
-
-def aggregate_score(results):
+def aggregate_score(prev_score, curr_label, curr_confidence):
     """Calculate aggregate attention score from classifications."""
 
-    # penalty for eaach kind of distraction
+    # penalty for eaach kind of distraction
     class_penalties = {0: 1,
-                       1: 10,
-                       2: 7,
-                       3: 10,
-                       4: 7,
-                       5: 6,
-                       6: 5,
-                       7: 15,
-                       8: 8,
-                       9: 5}
+                       1: 7,
+                       2: 6,
+                       3: 7,
+                       4: 6,
+                       5: 5,
+                       6: 4,
+                       7: 10,
+                       8: 7,
+                       9: 3}
 
-    # weight assigned to the last 5 measurements 
-    weights = {0: 1,
-               1: 0.8,
-               2: 0.6,
-               3: 0.4,
-               4: 0.2}
+    current_score = class_penalties[curr_label] * curr_confidence
+    aggregate_score = (current_score + prev_score) / 2
 
-    def shifted_sigmoid(x):
-        return (1/(1 + math.exp(-x+5)))
-
-    x = 0
-    for i in range(len(results)):
-        x += class_penalties[results[i][0]] * results[i][1] * weights[i]
-    x /= len(results)
-
-    attention_score = shifted_sigmoid(x)
-    return attention_score
+    return aggregate_score
